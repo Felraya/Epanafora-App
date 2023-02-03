f0ef301ca916d2db95a237318e0535a28bf463f0
Adrien Bernard <abernard@sigal.fr>
2023-02-03 10:55:29 +0100

    0.0.1

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const liveServer = require('live-server');
 // eslint-disable-next-line no-undef
 const player = require('play-sound')(opts = {})
+const colors = require('colors/safe');
+const pjson = require('./package.json');
 
+// Version
+console.log(colors.red(`\nVersion : ${pjson.version}`));
 
-player.play('./assets/rick-rolled.mp3',(err) =>{
+player.play('./assets/rick-rolled.mp3', { play: ['-v', 1 ]}, (err) =>{
   if (err) throw err
 })
 
-console.log("\nAslan est cringe !!!\n");
+console.log(colors.rainbow("\nAslan est cringe !!!\n"));
 
 const params = {
   port: 8181, // Set the server port. Defaults to 8080.
@@ -16,7 +20,6 @@ const params = {
   open: true, // When false, it won't load your browser by default.
   file: 'index.html', // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
   wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
-  mount: [['/components', './node_modules']], // Mount a directory to a route.
   logLevel: 1, // 0 = errors only, 1 = some, 2 = lots
 };
 liveServer.start(params);
